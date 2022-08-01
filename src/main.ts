d9f3ce364d4b39a2f81897544e96decfdb2c6e45
Gypsophila。 <futur_wxc@163.com>
2022-08-01 14:49:41 +0800

    chore: 安装 pinia，并引入

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { setupRouter } from '@/router';
+import { setupStore } from '@/store';
 
 const app = createApp(App);
 
 // 注册路由
 setupRouter(app);
+// 注册 pinia
+setupStore(app);
 
 app.mount('#app');
