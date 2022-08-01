d1420e8765520bf948510b05969fa57f9d88ce7d
Gypsophila。 <futur_wxc@163.com>
2022-08-01 11:04:02 +0800

    chore: 配置 @ 路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+/**
+ * 查看更多：
+ * https://vitejs.dev/config/
+ */
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      // @/xxxx => src/xxxx
+      {
+        find: /@\//,
+        replacement: pathResolve('src') + '/',
+      },
+    ],
+  },
 });
